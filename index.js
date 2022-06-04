c4a8a0db8396344b85737a38d2fdd0df14e9a5e7
manojgholap <manojgholap17021998@gmail.com>
2022-06-04 14:15:28 +0530

    save

--- index.js
+++ index.js
@@ -11,25 +11,25 @@ const garage = require('./routes/garage');
 const car = require('./routes/cars');
 const service = require('./routes/services');
 const order = require('./routes/order')
-const PORT = process.env.port || 80
-Database.loadDB((err, res) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log(res);
-    }
-})
+const PORT = process.env.port || 3200
+// Database.loadDB((err, res) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(res);
+//     }
+// })
 
 app.set('view engine', 'ejs');
 app.use(cors())
 // app.use(express.Router())
-app.use('/user', user);
-app.use('/login', login);
-app.use('/garage', garage);
-app.use('/car', car);
-app.use('/service', service);
-app.use('/order',order);
+// app.use('/user', user);
+// app.use('/login', login);
+// app.use('/garage', garage);
+// app.use('/car', car);
+// app.use('/service', service);
+// app.use('/order',order);
 app.use(compression())
 app.use(express.static(__dirname + '/uploads'));
 app.use(express.static(__dirname + '/brands'));
@@ -41,5 +41,5 @@ app.get('/', function(req, res) {
   });
 
 app.listen(PORT,() => {
-    console.log(`app is starting on port ${PORT}`);
+    console.log(`app is starting on port ${ PORT }`);
 })
